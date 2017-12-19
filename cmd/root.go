fce616ce736c27db4fe49bad3f1790a7d03455ee
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-12-19 22:00:30 +0900

    initial source

---
+++ cmd/root.go
@@ -0,0 +1,14 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gabel",
+	Short: "gabel is a tool for creating teacher data",
+}
+
+//Execute CLI
+func Execute() {
+}
