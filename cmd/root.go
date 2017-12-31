05da536e2906a2d4b96244b6b9ee763b06751887
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-12-31 23:57:39 +0900

    add run command

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,22 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
+//RootCmd is
 var rootCmd = &cobra.Command{
 	Use:   "gabel",
 	Short: "gabel is a tool for creating teacher data",
 }
 
-//Execute CLI
+//Execute Command
 func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
