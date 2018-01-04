c2fc72857872c2744dc8256e942437256a5e25c0
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-01-04 21:12:14 +0900

    fix

--- utils/iohelper.go
+++ utils/iohelper.go
@@ -4,9 +4,29 @@ import "os"
 
 //Mkdir generate directory of given path
 func Mkdir(path string) error {
-	if _, err := os.Stat(path); !os.IsNotExist(err) {
+	if ok := isExist(path); ok {
 		return nil
 	}
-
 	return os.Mkdir(path, os.ModePerm)
+}
+
+//CreateFile generate file of given path
+func CreateFile(name string, flag int) (*os.File, error) {
+	if ok := isExist(name); ok {
+		file, err := OpenFile(name, flag)
+		file.Truncate(0)
+		return file, err
+	}
+
+	return OpenFile(name, flag)
+}
+
+//OpenFile opens file
+func OpenFile(name string, flag int) (*os.File, error) {
+	return os.OpenFile(name, flag, os.ModePerm)
+}
+
+func isExist(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
 }
