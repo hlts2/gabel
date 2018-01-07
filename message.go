8cb5f242802272a2c85b3c21c2ca9e729d799409
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-01-07 21:33:51 +0900

    fix

--- message.go
+++ message.go
@@ -6,6 +6,6 @@ labelingMessageTmpl returns the colored message to be displayed
 ex)
 1
  「csv text」
-Please Input Your Label {exist == [0]} or {not exist == [1]} %
+Please Input Your Label {exist == [1]} or {not exist == [0]} %
 */
 var labelingMessageTmpl = "\n%d\n\x1b[33m 「%s」\x1b[0m\nPlease input label. \x1b[4m{%s == %v}\x1b[0m or \x1b[4m{%s == %v}\x1b[0m "
