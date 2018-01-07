e5c0cd94549224d0aa35f9f45eff8204b0d5974f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-01-07 16:03:38 +0900

    fix

--- gabel_test.go
+++ gabel_test.go
@@ -2,6 +2,17 @@ package gabel
 
 import "testing"
 
-func TestNewGabelio(t *testing.T) {
+const (
+	CSVPath = "example/example.csv"
+)
 
+func TestNewGabelio(t *testing.T) {
+	gio, err := NewGabelio(CSVPath)
+	if err != nil {
+		t.Errorf("NewGabelio(CSVPath) err is error: %v", err)
+	}
+
+	if gio == nil {
+		t.Errorf("newGabelio(CSVPath) gio is nil")
+	}
 }
