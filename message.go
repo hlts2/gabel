395ad66d99787f8d913930371e93d8c708f80bed
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-01-07 21:32:44 +0900

    fix

--- message.go
+++ message.go
@@ -1,3 +1,11 @@
 package gabel
 
+/*
+labelingMessageTmpl returns the colored message to be displayed
+
+ex)
+1
+ 「csv text」
+Please Input Your Label {exist == [0]} or {not exist == [1]} %
+*/
 var labelingMessageTmpl = "\n%d\n\x1b[33m 「%s」\x1b[0m\nPlease input label. \x1b[4m{%s == %v}\x1b[0m or \x1b[4m{%s == %v}\x1b[0m "
