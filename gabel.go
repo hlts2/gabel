7ab72f92599158e358ffd435927af88b0f9d0233
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-01-21 16:26:20 +0900

    fix

--- gabel.go
+++ gabel.go
@@ -41,17 +41,22 @@ func (g Gabel) Run(reader *csv.Reader, writer *csv.Writer) error {
 }
 
 func (g Gabel) labeling(id int, text string, writer *csv.Writer) {
+again:
 	fmt.Print(messageTmpl(id, text, g.Labels))
 	for {
 		in := g.StdIn.ReadLine()
 		if in == "mod" {
 			fmt.Print(modifyMessageTmpl(id, text, g.Labels))
-			continue
+
+			//TODO modify past label
+			goto again
 		}
 
-		sp, err := helpers.StringToIntSlice(in, ",")
+		//Convert comma-separated string to int slice
+		//"1, 2, 3" => []int{1, 2, 3}
+		isp, err := helpers.StringToIntSlice(in, ",")
 		if err == nil {
-			if helpers.IsContainsAllElement(g.Labels.GetValues(), sp) {
+			if helpers.IsContainsAllElement(g.Labels.GetValues(), isp) {
 				writer.Write([]string{text, in})
 				writer.Flush()
 				break
