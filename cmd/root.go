154f63eea60fc516f76695ba039319d5d45745d3
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-05-28 00:26:59 +0900

    delete unnecessary code

---
+++ cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gabel",
+	Short: "gabel is a CLI tool for creating teacher data",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+var helpText = `
+Usage:
+   gabel <option>
+
+Flags:
+   -h, --help  Help for Run Command
+               $ gabel run --help
+   -s, --set   Set config file path for gabel
+               $ gabel run -s config.yaml
+	`
+
+// Execute executes the CLI application
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
