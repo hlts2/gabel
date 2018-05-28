757c97c1abb958d564843664042e20807139bea5
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-05-28 20:08:50 +0900

    added cli option

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/hlts2/gabel/pkg/gabel"
 	"github.com/spf13/cobra"
 )
 
@@ -11,8 +12,28 @@ var rootCmd = &cobra.Command{
 	Use:   "gabel",
 	Short: "gabel is a CLI tool for creating teacher data",
 	RunE: func(cmd *cobra.Command, args []string) error {
+
+		_, err := gabel.NewCSV(configPath)
+		if err != nil {
+			return err
+		}
+
 		return nil
 	},
+}
+
+var configPath string
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&configPath, "set", "s", "", "set config file path")
+}
+
+// Execute executes the CLI application
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
 }
 
 var helpText = `
@@ -25,11 +46,3 @@ Flags:
    -s, --set   Set config file path
                $ gabel -s config.yaml
 	`
-
-// Execute executes the CLI application
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-}
